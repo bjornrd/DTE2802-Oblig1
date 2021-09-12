573462a48474b9d812cf927339cc57ceeb0d35e2
Bjørn Rudi Dahl <bjorn.rudi.dahl@outlook.com>
2021-09-12 19:51:01 +0200

    1.14 - Working as expected

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -4,13 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using WebApplication1.Models.Entities;
+
 namespace WebApplication1.Controllers
 {
     public class ProductController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello World";
+            List<Product> products = new List<Product>
+            {
+                new Product { Name = "Hammer", Price = 121.50m, Category = "Verktøy" },
+                new Product { Name = "Vinkelsliper", Price = 1520.00m, Category = "Verktøy" }, 
+                new Product { Name = "Melk", Price = 14.50m, Category = "Dagligvarer" }, 
+                new Product { Name = "Kjøttkaker", Price = 32.00m, Category = "Dagligvarer" }, 
+                new Product { Name = "Brød", Price = 25.50m, Category = "Dagligvarer" }
+            };
+            return View(products);
         }
     }
 }
